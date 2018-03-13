e04d82eafd1e03707f50f2c8278f2df9a157d898
TrungDT <thanhtrung.it25@gmail.com>
2018-03-13 22:45:22 +0700

    simple kanban app first commit

--- source/App.js
+++ source/App.js
@@ -1,26 +1,70 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import KanbanBoard from "./KanbanBoard";
 
-class GroceryList extends Component {
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the **whole** book",
+    color: "#BD8D31",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description:
+      "Code along with the samples in the book at [github](https://github.com/pro-react)",
+    color: "#3A7E28",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  }
+];
+
+class Search extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(
+      "Submitted values are: ",
+      event.target.name.value,
+      event.target.email.value
+    );
+  }
+
   render() {
     return (
-      <ul>
-        <GroceryItem quantity="1">Bread</GroceryItem>
-        <GroceryItem quantity="6">Eggs</GroceryItem>
-        <GroceryItem quantity="2">Milk</GroceryItem>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <div className="formGroup">
+          Name: <input type="text" name="name" />
+        </div>
+        <div className="formGroup">
+          Email: <input type="email" name="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
 
-class GroceryItem extends Component {
-  render() {
-    return (
-      <li>
-        {this.props.quantity}x {this.props.children}
-      </li>
-    );
-  }
-}
-
-ReactDOM.render(<GroceryList />, document.getElementById("root"));
+ReactDOM.render(
+  // <Search />,
+  <KanbanBoard cards={cardsList} />,
+  document.getElementById("root")
+);
